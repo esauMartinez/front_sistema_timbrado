bf76eb7e1c9e94530fe18d05b0b2999486535a3d
esauMartinez <you@example.com>
2022-02-23 14:11:18 -0600

    cambios en la url puerto

--- src/services/url.js
+++ src/services/url.js
@@ -1,4 +1,4 @@
-export const url = 'https://timbrado-cfdi.herokuapp.com/:3000/api/v1'
+export const url = 'https://timbrado-cfdi.herokuapp.com/api/v1'
 
 
-export const urlCp = 'https://timbrado-cfdi.herokuapp.com/:3000/api/v1'
+export const urlCp = 'https://timbrado-cfdi.herokuapp.com/api/v1'
