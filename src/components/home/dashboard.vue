84bed4a5f14b56797df5a082a5064d0abf1809f1
esauMartinez <you@example.com>
2022-03-11 13:35:44 -0600

    add dashboar component

---
+++ src/components/home/dashboard.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="dashboard">
+        <div class="container mt-5">
+            <h1 class="text-center">Dashboard</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dashboard'
+}
+</script>
+
+<style scoped>
+    .dashboard {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background-color: #dedfe0;
+        /* background-color: rgb(255, 255, 255) !important; */
+    }
+</style>
+
